8542190105e5e9b66f0688ebf204f5a9102bfc16
Jean-Louis Huynen <huynenjl@gmail.com>
2023-08-30 11:53:09 +0200

    chg: [api] submit urls to crawler

--- lib/ex_ail.ex
+++ lib/ex_ail.ex
@@ -7,11 +7,21 @@ defmodule ExAil do
 
   alias ExAil.Client
 
-  @type data() :: binary
-  @type metadata() :: String.t()
-  @type source() :: String.t()
-  @type source_uuid() :: String.t()
-  @type default_encoding() :: String.t()
+  @type data :: binary
+  @type metadata :: String.t()
+  @type source :: String.t()
+  @type source_uuid :: String.t()
+  @type default_encoding :: String.t()
+
+  @type url :: String.t()
+  @type har :: boolean()
+  @type screenshot :: boolean()
+  @type deph_limit :: non_neg_integer()
+  # frequency can be 'monthly', 'weekly', 'daily', 'hourly'
+  @type frequency :: String.t()
+  @type cookiejar :: String.t()
+  # proxy can be set to 'onion', 'tor' or 'force_tor'
+  @type proxy :: String.t()
 
   @spec base_location :: String.t()
   def base_location() do
@@ -56,9 +66,11 @@ defmodule ExAil do
     end
   end
 
-  @spec feed_json_item(data(), metadata(), source(), source_uuid(), default_encoding()) ::
+  @spec feed_json_item(data, metadata, source, source_uuid, default_encoding) ::
           {:error, {:client_error, any} | {:http_error, any} | {:server_error, any}} | {:ok, any}
-  def feed_json_item(data, metadata, source, source_uuid, default_encoding) do
+  def feed_json_item(data, metadata, source, source_uuid, default_encoding)
+      when is_binary(data) and is_binary(metadata) and is_binary(source) and
+             is_binary(source_uuid) and is_binary(default_encoding) do
     case source do
       "" ->
         {:error, "source can not be blank."}
@@ -103,10 +115,120 @@ defmodule ExAil do
     feed_json_item(data, metadata, source, source_uuid, default_encoding)
   end
 
+  @spec crawl_url(url, har, screenshot, deph_limit, frequency, cookiejar, proxy) ::
+          {:error, {:client_error, any} | {:http_error, any} | {:server_error, any}} | {:ok, any}
+  def crawl_url(
+        url,
+        har \\ false,
+        screenshot \\ false,
+        depth_limit \\ 1,
+        frequency \\ "",
+        cookiejar \\ "",
+        proxy \\ "force_tor"
+      )
+      when is_binary(url) and is_binary(cookiejar) and is_binary(proxy) and is_binary(frequency) and
+             is_boolean(screenshot) and is_boolean(har) and
+             (is_integer(depth_limit) and depth_limit > 0) do
+    dict = %{
+      "url" => url,
+      "har" => har,
+      "screenshot" => screenshot,
+      "depth_limit" => depth_limit
+    }
+
+    dict =
+      case frequency do
+        "" ->
+          dict
+
+        "monthly" ->
+          Map.put(dict, "frequency", "monthly")
+
+        "weekly" ->
+          Map.put(dict, "frequency", "weekly")
+
+        "daily" ->
+          Map.put(dict, "frequency", "monthly")
+
+        "hourly" ->
+          Map.put(dict, "frequency", "hourly")
+
+        _ ->
+          dict
+      end
+
+    dict =
+      case proxy do
+        "" ->
+          dict
+
+        "onion" ->
+          Map.put(dict, "proxy", "onion")
+
+        "tor" ->
+          Map.put(dict, "proxy", "tor")
+
+        "force_tor" ->
+          Map.put(dict, "proxy", "force_tor")
+
+        _ ->
+          dict
+      end
+
+    dict =
+      case check_url(proxy) do
+        {:ok} ->
+          Map.put(dict, "proxy", proxy)
+
+        _ ->
+          dict
+      end
+
+    dict =
+      case cookiejar do
+        "" ->
+          dict
+
+        _ ->
+          Map.put(dict, "cookiejar", cookiejar)
+      end
+
+    case Client.post("add/crawler/task", Jason.encode!(dict)) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: response_body}} ->
+        {:ok, response_body}
+
+      {:ok, %HTTPoison.Response{status_code: status_code, body: response_body}}
+      when status_code in 400..499 ->
+        {:error, {:client_error, response_body}}
+
+      {:ok, %HTTPoison.Response{status_code: status_code, body: response_body}}
+      when status_code in 500..599 ->
+        {:error, {:server_error, response_body}}
+
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        {:error, {:http_error, reason}}
+    end
+  end
+
   @spec encode_and_compress(data) :: data
   def encode_and_compress(data) do
     data
     |> :zlib.gzip()
     |> Base.encode64()
+  end
+
+  defp check_url(value) do
+    case URI.parse(value) do
+      %URI{scheme: nil} -> {:error, "Missing a scheme"}
+      %URI{host: nil} -> {:error, "Missing a host"}
+      %URI{host: host} -> validate_host(host)
+    end
+  end
+
+  defp validate_host(host) do
+    case :inet.gethostbyname(Kernel.to_charlist(host)) do
+      {:ok, _} -> {:ok}
+      {:error, _} -> {:error, "invalid host"}
+    end
   end
 end
